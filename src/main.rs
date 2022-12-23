41d287e841a5e649e4c37fb346a12fd646cda83c
ronnfran mac <ronnfeldt.frank@gmail.com>
2022-12-23 15:40:12 +1000

    prints to stderror instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Probelm passing arguments: {err}");
+        eprintln!("Probelm passing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error {e}");
+        eprintln!("Application error {e}");
         process::exit(1);
     }
-}
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn this_tests_runs() {
-        assert_eq!(2 + 2, 4);
-    }
-
 }
