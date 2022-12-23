57ede84272e058700179515418b1adaf5fd370b1
ronnfran mac <ronnfeldt.frank@gmail.com>
2022-12-23 14:28:29 +1000

    refactored code into src and lib files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
-std::env;
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 
 fn main() {
-    println!("This is Mini-grep!");
-
     let args: Vec<String> = env::args().collect();
-    dbg!(args)
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Probelm passing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error {e}");
+        process::exit(1);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn this_tests_runs() {
+        assert_eq!(2 + 2, 4);
+    }
+
 }
